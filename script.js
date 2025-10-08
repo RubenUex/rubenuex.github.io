689562e13ff8636b1922b42b73e19a9d98299b0c
RubenUex <rvazquezva@alumnos.unex.es>
2025-10-08 19:02:33 +0200

    Pintar basico

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function myFunction() {
 	alert("Hello, world!")
 }
 
-function draw() {
+
+let isDrawing = false
+let clientX = 0
+let clientY = 0
+
+function initCanvas() {
 	const canvas = document.getElementById("myCanvas")
 	if (!canvas) {
 		console.error("Canvas not found.")
@@ -13,13 +18,44 @@ function draw() {
 		console.error("No context for canvas found.")
 		return
 	}
+	const canvasRect = canvas.getBoundingClientRect()
 	context.moveTo(0, 0)
-	context.lineTo(50, 50)
-	context.stroke()
+	canvas.addEventListener("mousedown", (event) => {
+		isDrawing = true
+		doDrawing(event, canvas, context).then(() => console.log("Finished"))
+	});
+	canvas.addEventListener("mouseup", () => {
+		isDrawing = false
+	})
+
+}
+
+onmousemove = function(e){
+	clientX = e.clientX
+	clientY = e.clientY
+}
+
+async function sleep(ms) {
+  await new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function doDrawing(event, canvas, context) {
+	while (isDrawing) {
+		draw(event, canvas, context)
+		await sleep(10)
+	}
+}
+
+function draw(event, canvas, context) {
+	const canvasRect = canvas.getBoundingClientRect()
+	const canvasX = clientX - canvasRect.left
+	const canvasY = clientY - canvasRect.top	
+	context.fillRect(canvasX - 2, canvasY - 2, 4, 4)
+
 }
 
 function init() {
-	window.onload = (() => draw())
+	window.onload = (() => initCanvas())
 }
 
 init()
